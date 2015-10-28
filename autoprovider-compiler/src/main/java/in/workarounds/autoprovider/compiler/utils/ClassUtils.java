f62084c07cd1ee9393b3a71ca0c405f70573821c
mouleshwar <mouli.4542@gmail.com>
2015-10-29 00:14:23 +0530

    Add ClassUtils

--- autoprovider-compiler/src/main/java/in/workarounds/autoprovider/compiler/utils/ClassUtils.java
+++ autoprovider-compiler/src/main/java/in/workarounds/autoprovider/compiler/utils/ClassUtils.java
@@ -5,9 +5,10 @@ import com.squareup.javapoet.ClassName;
 /**
  * Created by mouli on 10/22/15.
  */
-public class ClassUtils {
+public interface ClassUtils {
 
-    public static final ClassName CURSOR = ClassName.get("android.database", "Cursor", new String[]{});
-    public static final ClassName NONNULL = ClassName.get("android.support.annotation", "NonNull", new String[]{});
+    ClassName CURSOR = ClassName.get("android.database", "Cursor");
+    ClassName NONNULL = ClassName.get("android.support.annotation", "NonNull");
+    ClassName ABSTRACT_CURSOR = ClassName.get("in.workarounds.autoprovider", "AbstractCursor");
 
 }
