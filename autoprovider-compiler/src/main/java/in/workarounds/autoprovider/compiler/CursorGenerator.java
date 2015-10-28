dad6020d8e4f498d10d3634d1d6a99e6759fc95a
mouleshwar <mouli.4542@gmail.com>
2015-10-29 00:16:25 +0530

    Add GET_OBJECT method to CursorGenerator

--- autoprovider-compiler/src/main/java/in/workarounds/autoprovider/compiler/CursorGenerator.java
+++ autoprovider-compiler/src/main/java/in/workarounds/autoprovider/compiler/CursorGenerator.java
@@ -10,33 +10,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
 
 import in.workarounds.autoprovider.compiler.utils.ClassUtils;
 import in.workarounds.autoprovider.compiler.utils.StringUtils;
+
+import static in.workarounds.autoprovider.compiler.utils.ClassUtils.*;
 
 /**
  * Created by mouli on 10/22/15.
  */
 public class CursorGenerator {
 
+    private final String mGetObject = "getObject";
+
     private final String ERROR_MESSAGE_NULL = "The value of '%s' in the database was null, which is not allowed according to the model definition";
-    private final ClassName ABSTRACT_CURSOR = ClassName.get("in.workarounds.autoprovider", "AbstractCursor");
 
     private final MethodSpec CONSTRUCTOR;
     List<MethodSpec> methods = new ArrayList<>();
+    private final MethodSpec GET_OBJECT;
 
     public CursorGenerator(AnnotatedTable annotatedTable) {
 
         CONSTRUCTOR = MethodSpec.constructorBuilder()
                 .addModifiers(Modifier.PUBLIC)
-                .addParameter(ClassUtils.CURSOR, "cursor")
+                .addParameter(CURSOR, "cursor")
                 .addStatement("super(cursor)")
                 .build();
 
         for(AnnotatedColumn annotatedColumn: annotatedTable.getColumns()) {
             MethodSpec.Builder builder = MethodSpec.methodBuilder(String.format("get%s",
                     StringUtils.toCamelCase(annotatedColumn.getColumnName())))
-                    .addAnnotation(ClassUtils.NONNULL)
+                    .addAnnotation(NONNULL)
                     .addModifiers(Modifier.PUBLIC)
                     .returns(ClassName.get(annotatedColumn.getTypeInObject()))
                     .addCode(getColumnInitializer(annotatedTable.getTableName(), annotatedColumn));
@@ -50,6 +55,13 @@ public class CursorGenerator {
             builder.addStatement("return res");
             methods.add(builder.build());
         }
+
+        GET_OBJECT = MethodSpec.methodBuilder(mGetObject)
+                .addModifiers(Modifier.PUBLIC)
+                .returns(ClassName.get(annotatedTable.getAnnotatedClassElement().asType()))
+                .addCode(getObjectCode(annotatedTable))
+                .build();
+
     }
 
     private CodeBlock getColumnInitializer(String tableName, AnnotatedColumn annotatedColumn) {
@@ -61,6 +73,18 @@ public class CursorGenerator {
                         StringUtils.toCamelCase(simpleTypeName), StringUtils.toCamelCase(tableName), annotatedColumn.getColumnName().toUpperCase())
                 .build();
         return codeBlock;
+    }
+
+    private CodeBlock getObjectCode(AnnotatedTable annotatedTable) {
+        TypeMirror object = annotatedTable.getAnnotatedClassElement().asType();
+        CodeBlock.Builder builder = CodeBlock.builder()
+                .add("$T item = new $T();\n", object, object);
+        for(AnnotatedColumn annotatedColumn: annotatedTable.getColumns()) {
+            String columnName = annotatedColumn.getColumnName();
+            builder.add("item.$L = get$L();\n", columnName, StringUtils.toCamelCase(columnName));
+        }
+        builder.add("return item;\n");
+        return builder.build();
     }
 
     public JavaFile generateTable(String outputPackage, String outputName) {
@@ -76,6 +100,8 @@ public class CursorGenerator {
         builder.addMethod(CONSTRUCTOR);
 
         methods.forEach(builder::addMethod);
+
+        builder.addMethod(GET_OBJECT);
 
         return builder.build();
     }
