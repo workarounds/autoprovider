3dc8f5448bfc7f7456a7ef2da4522e267912799b
mouleshwar <mouli.4542@gmail.com>
2015-10-28 23:41:06 +0530

    Change returnType of abstract getId() to Long

--- autoprovider/src/main/java/in/workarounds/autoprovider/AbstractCursor.java
+++ autoprovider/src/main/java/in/workarounds/autoprovider/AbstractCursor.java
@@ -14,7 +14,7 @@ public abstract class AbstractCursor extends CursorWrapper {
         mColumnIndexes = new HashMap<String, Integer>(cursor.getColumnCount() * 4 / 3, .75f);
     }
 
-    public abstract long getId();
+    public abstract Long getId();
 
     protected int getCachedColumnIndexOrThrow(String colName) {
         Integer index = mColumnIndexes.get(colName);
